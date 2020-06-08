a2e374724545c4a42a884c55ce12f18cc4282e5b
absartaqvi <45893345+absartaqvi@users.noreply.github.com>
2020-06-08 22:36:10 +1000

    Update 6_QueueReconstructionByHeight.py

--- 6_QueueReconstructionByHeight.py
+++ 6_QueueReconstructionByHeight.py
@@ -1,5 +1,7 @@
 '''
-Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), 
+where h is the height of the person and k is the number of people in front of this person who have a height greater than or 
+equal to h. Write an algorithm to reconstruct the queue.
 
 Note:
 The number of people is less than 1,100.
@@ -35,7 +37,9 @@ Since I was thinking in the above way, I was not able to successfully solve the 
 the algorithm is, at any index we do not care if the current value is smaller. An index only cares if someone with
 a greater height has been seen. But a smaller height index cares about how many greater height indices are in front of it.
 
-I am not sure if I can do it any other way besides using the built-in `sorted` method. We first sorted people in descending order by height. So we get this:
+I am not sure if I can do it any other way besides using the built-in `sorted` method. We first sorted people in 
+descending order by height. So we get this:
 [[7, 0], [7, 1], [6, 1], [5, 0], [5, 2], [4, 4]]
-Now when we insert by index, this means the smallest one will get bubbled to the position equal to its index. And this approach explains I was half way there :) But, half way there is not good enough ;)
+Now when we insert by index, this means the smallest one will get bubbled to the position equal to its index.
+And this approach explains I was half way there :) But, half way there is not good enough ;)
 '''
